6ef37251d03783d7a638492aab9f453e4c76a263
Kostianyn <odessavshok@gmail.com>
2021-09-08 02:31:07 +0300

    task-01

--- js/task-01.js
+++ js/task-01.js
@@ -8,3 +8,13 @@
 
 // Категория: Животные
 // Количество элементов: 4
+
+const itemsRef = document.querySelectorAll('.item');
+console.log(`В списке ${itemsRef.length} категории`);
+
+itemsRef.forEach(item => {
+  const titleRef = item.querySelector('h2').textContent;
+  console.log(`
+Категория: ${titleRef}
+Количество элементов: ${item.querySelectorAll('li').length}`);
+});
