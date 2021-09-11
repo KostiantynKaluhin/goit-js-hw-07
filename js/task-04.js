de430b6064f6c1a3dfcb000d0ae8f21a9e7d2c77
Kostianyn <odessavshok@gmail.com>
2021-09-11 19:01:26 +0300

    task-04

--- js/task-04.js
+++ js/task-04.js
@@ -4,4 +4,28 @@
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
-const counterValue = 0
+const valueRef = document.querySelector('#value');
+
+let counterValue = 0;
+
+const btnIncrementRef = document.querySelector(
+  '#counter button[data-action="increment"]',
+);
+
+btnIncrementRef.addEventListener('click', onIncrementClick);
+
+function onIncrementClick(click) {
+  counterValue += 1;
+  valueRef.textContent = counterValue;
+}
+
+const btnDecrementRef = document.querySelector(
+  '#counter button[data-action="decrement"]',
+);
+
+btnDecrementRef.addEventListener('click', onDecrementClick);
+
+function onDecrementClick(click) {
+  counterValue -= 1;
+  valueRef.textContent = counterValue;
+}
